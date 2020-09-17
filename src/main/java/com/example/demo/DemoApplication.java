2c50c66576401e162e22fea8e6b594d24f62d97a
Master <40629009+munnikedost@users.noreply.github.com>
2020-09-18 01:38:51 +0530

    Update DemoApplication.java
    Add another line

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		System.out.println("Feature 2");
+		System.out.println("Feature 2 yay");
 	}
 
 }
